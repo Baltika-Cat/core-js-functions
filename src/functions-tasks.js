ab51cad0891131dd26feebfb8cb37a1b9083938f
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2024-04-03 02:35:48 +0300

    feat: complete all tasks

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -207,8 +207,11 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args2) => {
+    const args = args1.concat(args2);
+    return fn(...args);
+  };
 }
 
 /**
@@ -228,8 +231,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  const cache = {};
+  return () => {
+    cache[startFrom] =
+      cache[startFrom] !== undefined ? cache[startFrom] + 1 : startFrom;
+    return cache[startFrom];
+  };
 }
 
 module.exports = {
